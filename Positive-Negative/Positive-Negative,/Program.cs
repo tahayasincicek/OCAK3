22124defc7ad9a245be06c75bc595a49d2fe606d
Taha Yasin Çiçek <116304917+tahayasincicek@users.noreply.github.com>
2025-03-11 22:53:08 +0300

    Add files via upload

---
+++ Positive-Negative/Positive-Negative,/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        
+        if (number > 0)
+        {
+            Console.WriteLine("The number is positive.");
+        }
+        else if (number < 0)
+        {
+            Console.WriteLine("The number is negative.");
+        }
+        else
+        {
+            Console.WriteLine("The number is zero.");
+        }
+    }
+}
